595ac9b2f38ecd429b933d73310cfa83455a6809
Lukas Stuppacher <magicluks@live.at>
2024-07-24 11:42:38 +0200

    v.0.7.0 - U7

--- api/src/controller/NavigationController.php
+++ api/src/controller/NavigationController.php
@@ -1,5 +1,6 @@
 <?php
 namespace deckbuilder_archive_spa_version_vue\api\controller;
+
 use deckbuilder_archive_spa_version_vue\api\views\JsonView;
 
 class NavigationController
@@ -19,8 +20,11 @@ class NavigationController
     }
 
     public function route(){
+        $this->setCorsHeaders();
+        
         $action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING);
         switch(strtolower($action)){
+            // Kartenbezogene Routen
             case 'filterallcards':
             case 'filterbyid':
             case 'filterbyname':
@@ -31,6 +35,7 @@ class NavigationController
             case 'filterbycost':
                 $this->searchForCard();
                 break;
+            // Deckbezogene Routen
             case 'searchbyuser':
             case 'searchbyname':
             case 'searchbyformat':
@@ -39,6 +44,7 @@ class NavigationController
             case 'displaydeckcontents':
                 $this->searchForDeck();
                 break;    
+            // Benutzerbezogene Routen
             case 'createarchiver':
             case 'loginarchiver':
             case 'logoutarchiver':
@@ -48,6 +54,7 @@ class NavigationController
             case 'deletearchiver':
                 $this->handleAccount();
                 break;
+            // Deckbau Routen
             case 'createdeck':
             case 'addcard':
             case 'removecard':
@@ -58,20 +65,42 @@ class NavigationController
                 $this->buildDeck();
                 break;    
             default:
-            $errorM = "Unknown Action";
-            $this->jsonView->display($errorM);                                
+                $errorM = "Unknown Action";
+                $this->jsonView->display($errorM);                                
+        }
+    }
+
+    private function setCorsHeaders() {
+        $allowedOrigins = [
+            'http://localhost:8080', // URL der Vue-Entwicklungsumgebung
+            'http://localhost/deckbuilder_archive_spa_version_vue', // URL der Produktionsumgebung
+        ];
+        if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowedOrigins)) {
+            header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+            header('Access-Control-Allow-Headers: Authorization, Content-Type, Accept');
+            header('Access-Control-Allow-Credentials: true');
+        }
+
+        // Preflight-Anfragen behandeln
+        if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+            header('HTTP/1.1 200 OK');
+            exit();
         }
     }
 
     private function searchForCard(){
         $this->cardController->route();
     }
+
     private function searchForDeck(){
         $this->deckController->route();
     }
+
     private function handleAccount(){
         $this->accountController->route();
     }
+
     private function buildDeck(){
         $this->builderController->route();
     }
